fafbe45ce6a797395422579a97346d3b59d1072c
Ishita Mediratta <ishitamediratta19@gmail.com>
2021-04-28 09:11:44 -0400

    added oracle vilbert

--- oracle_vilbert.py
+++ oracle_vilbert.py
@@ -36,6 +36,7 @@ import csv
 import argparse
 import glob
 from types import SimpleNamespace
+import pandas as pd
 import ipdb
 st = ipdb.set_trace
 
@@ -217,9 +218,6 @@ class IterDataset2d(Dataset):
         self.split = split
         self.annos = self.load_annos()
         print('Loaded %d samples' % len(self.annos))
-        self.tokenizer = BertTokenizer.from_pretrained(
-            "bert-base-uncased", do_lower_case=True
-        )
 
     def load_annos(self):
         """Load annotations."""
@@ -243,45 +241,19 @@ class IterDataset2d(Dataset):
         obj_ids = self.get_object_ids(anno)
         obj_names = self.get_object_names(anno)
         preds = self.get_predicate_names(anno)
-        queries = []
         gt_spatials = []
-        input_masks = []
-        segment_id_list = []
         og_sentences = []
         for idx in range(num_pairs):
             query = obj_names[pairs[idx][0]] + " " + preds[idx] + " " + obj_names[pairs[idx][1]]
             og_sentences.append(query)
             gt_spatials.append(gt_boxes[pairs[idx][0]])
-            
-            tokens = self.tokenizer.encode(query)
-            tokens = self.tokenizer.add_special_tokens_single_sentence(tokens)
-
-            segment_ids = [0] * len(tokens)
-            input_mask = [1] * len(tokens)
-
-            max_length = 37
-            if len(tokens) < max_length:
-                # Note here we pad in front of the sentence
-                padding = [0] * (max_length - len(tokens))
-                tokens = tokens + padding
-                input_mask += padding
-                segment_ids += padding
-                
-            queries.append(tokens)
-            input_masks.append(input_mask)
-            segment_id_list.append(segment_ids)
-        
         
         return {
             "filename": anno['filename'],
             "file_path": os.path.join(self.im_path, anno['filename']),
-            "num_queries": len(queries),
-            # "scene": self._load_image(anno['filename']),
+            "num_queries": len(og_sentences),
             "og_sentences": og_sentences,
-            "object_boxes": torch.from_numpy(np.asarray(gt_spatials)).float(),
-            "queries": torch.from_numpy(np.asarray(queries)),
-            "input_masks": torch.from_numpy(np.asarray(input_masks)),
-            "segment_ids": torch.from_numpy(np.asarray(segment_id_list))
+            "object_boxes": torch.from_numpy(np.asarray(gt_spatials)).float()
         }
 
     def __len__(self):
@@ -329,98 +301,65 @@ class IterDataset2d(Dataset):
         """Return predicate classes for given image."""
         return anno['relations']['names']
 
-def tokenize_batch(batch):
-    return [tokenizer.convert_tokens_to_ids(sent) for sent in batch]
 
-def untokenize_batch(batch):
-    return [tokenizer.convert_ids_to_tokens(sent) for sent in batch]
-
-def detokenize(sent):
-    """ Roughly detokenizes (mainly undoes wordpiece) """
-    new_sent = []
-    for i, tok in enumerate(sent):
-        if tok.startswith("##"):
-            new_sent[len(new_sent) - 1] = new_sent[len(new_sent) - 1] + tok[2:]
-        else:
-            new_sent.append(tok)
-    return new_sent
-
-def printer(sent, should_detokenize=True):
-    if should_detokenize:
-        sent = detokenize(sent)[1:-1]
-    print(" ".join(sent))
-
-
-def process_feats(features, infos):
+def get_spatials(infos):
     num_image = len(infos)
-    max_length = 37
-    feature_list = []
     image_location_list = []
-    image_mask_list = []
     for i in range(num_image):
-        image_w = infos[i]['image_width']
-        image_h = infos[i]['image_height']
-        feature = features[i]
-        num_boxes = feature.shape[0]
-
-        g_feat = torch.sum(feature, dim=0) / num_boxes
-        num_boxes = num_boxes + 1
-        feature = torch.cat([g_feat.view(1,-1), feature], dim=0)
         boxes = infos[i]['bbox']
-        image_location = np.zeros((boxes.shape[0], 5), dtype=np.float32)
-        image_location[:,:4] = boxes
-        image_location[:,4] = (image_location[:,3] - image_location[:,1]) * (image_location[:,2] - image_location[:,0]) / (float(image_w) * float(image_h))
-        image_location[:,0] = image_location[:,0] / float(image_w)
-        image_location[:,1] = image_location[:,1] / float(image_h)
-        image_location[:,2] = image_location[:,2] / float(image_w)
-        image_location[:,3] = image_location[:,3] / float(image_h)
-        g_location = np.array([0,0,1,1,1])
-        image_location = np.concatenate([np.expand_dims(g_location, axis=0), image_location], axis=0)
-        image_mask = [1] * (int(num_boxes))
+        image_location_list.append(torch.tensor(boxes))
 
-        feature_list.append(feature)
-        image_location_list.append(torch.tensor(image_location))
-        image_mask_list.append(torch.tensor(image_mask))
-
-    features = torch.stack(feature_list, dim=0).float().cuda()
     spatials = torch.stack(image_location_list, dim=0).float().cuda()
-    image_mask = torch.stack(image_mask_list, dim=0).byte().cuda()
-    co_attention_mask = torch.zeros((num_image, num_boxes, max_length)).cuda()
-    return features, spatials, image_mask, co_attention_mask
+    return spatials
 
-def _iou2d(box_a, box_b):
-    intersection = _intersect(box_a, box_b)
-    vol_a = _area(box_a)
-    vol_b = _area(box_b)
-    union = vol_a + vol_b - intersection
-    return intersection / union
+def iou(anchors, gt_boxes):
+    """
+    anchors: (N, 4) ndarray of float
+    gt_boxes: (K, 4) ndarray of float
+    overlaps: (N, K) ndarray of overlap between boxes and query_boxes
+    """
+    N = anchors.size(0)
+    K = gt_boxes.size(0)
 
+    gt_boxes_area = (
+        (gt_boxes[:, 2] - gt_boxes[:, 0] + 1) * (gt_boxes[:, 3] - gt_boxes[:, 1] + 1)
+    ).view(1, K)
 
-def _intersect(box_a, box_b):
-    xA = max(box_a[0], box_b[0])
-    yA = max(box_a[1], box_b[1])
-    xB = min(box_a[2], box_b[2])
-    yB = min(box_a[3], box_b[3])
-    return max(0, xB - xA) * max(0, yB - yA)
+    anchors_area = (
+        (anchors[:, 2] - anchors[:, 0] + 1) * (anchors[:, 3] - anchors[:, 1] + 1)
+    ).view(N, 1)
 
+    boxes = anchors.view(N, 1, 4).expand(N, K, 4)
+    query_boxes = gt_boxes.view(1, K, 4).expand(N, K, 4)
 
-def _area(box):
-    return (box[2] - box[0]) * (box[3] - box[1])
+    iw = (
+        torch.min(boxes[:, :, 2], query_boxes[:, :, 2])
+        - torch.max(boxes[:, :, 0], query_boxes[:, :, 0])
+        + 1
+    )
+    iw[iw < 0] = 0
+
+    ih = (
+        torch.min(boxes[:, :, 3], query_boxes[:, :, 3])
+        - torch.max(boxes[:, :, 1], query_boxes[:, :, 1])
+        + 1
+    )
+    ih[ih < 0] = 0
+
+    ua = anchors_area + gt_boxes_area - (iw * ih)
+    overlaps = iw * ih / ua
+
+    return overlaps
 
 def iter2d_collate_fn(batch):
     """Collate function for scene-aware ScanNet object classification."""
     total_objects = sum([ex["num_queries"] for ex in batch])
     return {
         "file_path": [ex["file_path"] for ex in batch],
-        # "scene": torch.stack([ex["scene"] for ex in batch]),
         "filename": [ex["filename"] for ex in batch],
         "num_queries": [ex["num_queries"] for ex in batch],
-        "og_sentences": [item for ex in batch for item in ex["og_sentences"]],
-        "task": torch.ones((total_objects,1)) * 11,
-        "object_boxes": torch.cat([ex["object_boxes"] for ex in batch]),
-        "queries": torch.cat([ex["queries"] for ex in batch]),
-        "input_masks": torch.cat([ex["input_masks"] for ex in batch]),
-        "segment_ids": torch.cat([ex["segment_ids"] for ex in batch]),
+        "og_sentences": [ex["og_sentences"] for ex in batch],
+        "object_boxes": [ex["object_boxes"] for ex in batch]
     }
 
 # =============================
@@ -428,69 +367,60 @@ def iter2d_collate_fn(batch):
 # =============================
 feature_extractor = FeatureExtractor()
 
-args = SimpleNamespace(from_pretrained= "save/multitask_model/pytorch_model_9.bin",
-                       bert_model="bert-base-uncased",
-                       config_file="config/bert_base_6layer_6conect.json",
-                       max_seq_length=101,
-                       train_batch_size=1,
-                       do_lower_case=True,
-                       predict_feature=False,
-                       seed=42,
-                       num_workers=0,
-                       baseline=False,
-                       img_weight=1,
-                       distributed=False,
-                       objective=1,
-                       visual_target=0,
-                       dynamic_attention=False,
-                       task_specific_tokens=True,
-                       tasks='1',
-                       save_name='',
-                       in_memory=False,
-                       batch_size=1,
-                       local_rank=-1,
-                       split='mteval',
-                       clean_train_sets=True
-                      )
+# args = SimpleNamespace(from_pretrained= "save/multitask_model/pytorch_model_9.bin",
+#                        bert_model="bert-base-uncased",
+#                        config_file="config/bert_base_6layer_6conect.json",
+#                        max_seq_length=101,
+#                        train_batch_size=1,
+#                        do_lower_case=True,
+#                        predict_feature=False,
+#                        seed=42,
+#                        num_workers=0,
+#                        baseline=False,
+#                        img_weight=1,
+#                        distributed=False,
+#                        objective=1,
+#                        visual_target=0,
+#                        dynamic_attention=False,
+#                        task_specific_tokens=True,
+#                        tasks='1',
+#                        save_name='',
+#                        in_memory=False,
+#                        batch_size=1,
+#                        local_rank=-1,
+#                        split='mteval',
+#                        clean_train_sets=True
+#                       )
 
-config = BertConfig.from_json_file(args.config_file)
-with open('./vilbert_tasks.yml', 'r') as f:
-    task_cfg = edict(yaml.safe_load(f))
+# config = BertConfig.from_json_file(args.config_file)
+# with open('./vilbert_tasks.yml', 'r') as f:
+#     task_cfg = edict(yaml.safe_load(f))
 
-task_names = []
-for i, task_id in enumerate(args.tasks.split('-')):
-    task = 'TASK' + task_id
-    name = task_cfg[task]['name']
-    task_names.append(name)
+# task_names = []
+# for i, task_id in enumerate(args.tasks.split('-')):
+#     task = 'TASK' + task_id
+#     name = task_cfg[task]['name']
+#     task_names.append(name)
 
-timeStamp = args.from_pretrained.split('/')[-1] + '-' + args.save_name
-config = BertConfig.from_json_file(args.config_file)
-default_gpu=True
+# timeStamp = args.from_pretrained.split('/')[-1] + '-' + args.save_name
+# config = BertConfig.from_json_file(args.config_file)
+# default_gpu=True
 
-if args.predict_feature:
-    config.v_target_size = 2048
-    config.predict_feature = True
-else:
-    config.v_target_size = 1601
-    config.predict_feature = False
+# if args.predict_feature:
+#     config.v_target_size = 2048
+#     config.predict_feature = True
+# else:
+#     config.v_target_size = 1601
+#     config.predict_feature = False
 
-if args.task_specific_tokens:
-    config.task_specific_tokens = True    
+# if args.task_specific_tokens:
+#     config.task_specific_tokens = True    
 
-if args.dynamic_attention:
-    config.dynamic_attention = True
+# if args.dynamic_attention:
+#     config.dynamic_attention = True
 
-config.visualization = True
-num_labels = 3129
-
-
-model = VILBertForVLTasks.from_pretrained(
-    args.from_pretrained, config=config, num_labels=num_labels, default_gpu=default_gpu
-)
-    
-model.eval()
-cuda = torch.cuda.is_available()
-model = model.cuda()
+# config.visualization = True
+# num_labels = 3129
 
 
 dataloader = DataLoader(
@@ -503,60 +433,36 @@ dataloader = DataLoader(
         )
 
 
-list1=['filename','query','pred_bbox_subject','iou']
-with open("top1_results_task11.csv", "a") as fp:
+list1=['filename','query','pred_bbox_x1','pred_bbox_y1','pred_bbox_x2','pred_bbox_y2','max_iou', 'correct']
+with open("oracle_vilbert.csv", "a") as fp:
     wr = csv.writer(fp, dialect='excel')
     wr.writerow(list1)
 
-num_correct = 0
+num_correct_3 = 0
+num_correct_5 = 0
+num_correct_7 = 0
 num_examples = 0
 
 with torch.no_grad():
     for step, batch in enumerate(dataloader):
         if step%10==0 and step>0:
             print(step)
-        features, infos = feature_extractor.extract_features(batch["file_path"])
-        features, spatials, image_mask, co_attention_mask = process_feats(features, infos)
+        _, infos = feature_extractor.extract_features(batch["file_path"])
+        spatials = get_spatials(infos) # B x 100 x 4
+        for curr_b in range(BATCH_SIZE):
+            gt_boxes = batch["object_boxes"][curr_b].cuda() # NUM_QUERIES x 4
+            iou_matrix = iou(spatials[curr_b], gt_boxes) # 100 x NUM_QUERIES
+            max_iou_per_query, max_iou_per_query_idx = torch.max(iou_matrix, axis=0) # NUM_QUERIES
+            num_queries = max_iou_per_query.shape[0]
+            pred_boxes = spatials[curr_b][max_iou_per_query_idx, :].detach().cpu().numpy() # NUM_QUERIES x 4
+            stats = pd.DataFrame({'filename': np.repeat(batch["filename"][curr_b], num_queries), 'query': batch["og_sentences"][curr_b], 'pred_bbox_x1':pred_boxes[:,0],'pred_bbox_y1':pred_boxes[:,1],'pred_bbox_x2':pred_boxes[:,2],'pred_bbox_y2':pred_boxes[:,3], "max_iou": max_iou_per_query.detach().cpu().numpy()}, columns=['filename','query','pred_bbox_x1','pred_bbox_y1','pred_bbox_x2','pred_bbox_y2','max_iou'])
+            stats.to_csv('oracle_vilbert.csv', mode='a', header=False, index=False)
+            num_correct_3 += (torch.count_nonzero((max_iou_per_query > 0.3)))
+            num_correct_5 += (torch.count_nonzero((max_iou_per_query > 0.5)))
+            num_correct_7 += (torch.count_nonzero((max_iou_per_query > 0.7)))
+            num_examples += num_queries
 
-        num_queries = torch.tensor(batch["num_queries"]).cuda()
-        og_idx = np.repeat(np.arange(0, BATCH_SIZE), batch["num_queries"])
-        features = torch.repeat_interleave(features, num_queries, dim=0)
-        spatials = torch.repeat_interleave(spatials, num_queries, dim=0)
-        image_mask = torch.repeat_interleave(image_mask, num_queries, dim=0)
-        co_attention_mask = torch.repeat_interleave(co_attention_mask, num_queries, dim=0)
-        
-        try:
-            _, _, _, _, _, _, vision_logit, _, _, _ = model(
-                batch["queries"].cuda(), features, spatials, batch["segment_ids"].cuda(), batch["input_masks"].cuda(), image_mask, co_attention_mask, torch.Tensor(batch["task"]).long().cuda(), output_all_attention_masks=True
-            )
-            assert vision_logit.size()[0] == features.size()[0]
-        except:
-            print("Uggggghhhhh")
-            print(step)
-            print(batch["file_path"])
-            print(vision_logit.size(), features.size())
-            print(num_queries)
-            st()
-            print(features.size()[0])
             
-        stats = []
-        for curr_idx in range(features.size()[0]):
-            height, width = infos[og_idx[curr_idx]]["image_height"], infos[og_idx[curr_idx]]["image_width"]
-
-            _, grounding_idx = torch.sort(vision_logit[curr_idx].view(-1), 0, True)
-            box = spatials[curr_idx][grounding_idx[0]][:4].tolist()
-            y1 = int(box[1] * height)
-            y2 = int(box[3] * height)
-            x1 = int(box[0] * width)
-            x2 = int(box[2] * width)
-            pred_box = [x1, y1, x2, y2]
-            calc_iou = _iou2d(batch["object_boxes"][curr_idx], pred_box)
-            if calc_iou > 0.3:
-                num_correct += 1
-            num_examples += 1
-            stats.append([batch["file_path"][og_idx[curr_idx]], batch["og_sentences"][curr_idx], pred_box, calc_iou])
-        with open("top1_results_task11.csv", "a") as f:
-            writer = csv.writer(f)
-            writer.writerows(stats)
-
-print("Top 1 Accuracy: {}".format(num_correct/num_examples))
+print("Accuracy @ 0.3: {}".format(num_correct_3.item()/num_examples))
+print("Accuracy @ 0.5: {}".format(num_correct_5.item()/num_examples))
+print("Accuracy @ 0.7: {}".format(num_correct_7.item()/num_examples))
